34e27b81d9467e2f6053d9705b43270dcea57fe1
Pawel Bogut <pawel.bogut@gmail.com>
2017-06-10 23:59:31 +0100

    Add magerun based on mage-php7-fpm container

---
+++ magerun/php7/n98-magerun.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#=================================================
+# name:   n98-magerun.sh
+# author: Pawel Bogut <http://pbogut.me>
+# date:   10/06/2017
+#=================================================
+cd $MAGENTO_PATH
+n98-magerun "$@"
